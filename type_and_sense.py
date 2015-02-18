b964db3e1bec2affd13b8666c976325c6f831fbe
Sumanth Prabhu <sumanthprabhu.104@gmail.com>
2015-02-18 18:16:45 +0530

    readme

--- type_and_sense.py
+++ type_and_sense.py
@@ -7,23 +7,27 @@ def main(argv):
 	
 	with open(argv[1], "rb") as input_data:
 
-		term = input_data.readline()
+		term = input_data.readline().strip()
 		neighbours = input_data.readlines()
 
 		candidates = []
 
 		for neighbour in neighbours:
+			neighbour = neighbour.strip()
 
 			candidates.append(
 				x.lowest_common_hypernyms(y) for x in wn.synsets(term) \
 				for y in wn.synsets(neighbour)
 				)
+
 		candidates = [val for sublist in candidates for val in sublist]
+		candidates = [val for sublist in candidates for val in sublist]
+
 		counts = Counter(candidates)
 		candidates = sorted(candidates, key=counts.get, reverse= True)
 
 		if candidates:
-			print candidates[0]
+			print candidates[0].lemma_names()
 		
 if __name__ == "__main__":
 	main(sys.argv)
